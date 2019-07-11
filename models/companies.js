3243678d593a32f8da8d332841129d0f134f860c
Joanne Badua <joannebadua@gmail.com>
2019-07-11 15:16:32 -0700

    Add Doctype, Head, Body, and scriptags to Main.Handlebars

---
+++ models/companies.js
@@ -0,0 +1,71 @@
+module.exports = function (sequelize, DataTypes) {
+    var Companies = sequelize.define("companies", {
+        company_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1 - 50]
+            }
+
+        },
+
+        average_rating: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                len: [1 - 10]
+            }
+        },
+
+        average_pay_per_hour: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                len: [1 - 20]
+            }
+        },
+
+        number_reviews: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1 - 20]
+            }
+        }
+    });
+
+    // Here we want to associate the model Companies with the model Reviews
+    Companies.associate = function (models) {
+
+        // Companies have many reviews
+        Companies.hasMany(models.reviews, {
+            onDelete: "cascade"
+        });
+    };
+
+    Companies.create({
+        company_name: "Uber",
+        average_rating: '3',
+        average_pay_per_hour: '15',
+        number_reviews: '0'
+    })
+
+    Companies.create({
+        company_name: "Lyft",
+        average_rating: '3',
+        average_pay_per_hour: '15',
+        number_reviews: '0'
+    })
+
+    Companies.create({
+        company_name: "Lugg",
+        average_rating: '3',
+        average_pay_per_hour: '15',
+        number_reviews: '0'
+    })
+
+    return Companies;
+}
+
+
+
---
+++ models/reviews.js
@@ -0,0 +1,62 @@
+module.exports = function (sequelize, DataTypes) {
+    var Reviews = sequelize.define("reviews", {
+        company_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1 - 30]
+            }
+        },
+
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1 - 30]
+            }
+        },
+
+        pay_per_hour: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                len: [1 - 30]
+            }
+        },
+
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1 - 10]
+            }
+        }
+    });
+
+    // Associate Reviews model with Companies
+    Reviews.associate = function (models) {
+
+        // The model Reviews belongs to Companies
+        Reviews.belongsTo(models.companies, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        Reviews.belongsTo(models.users, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+    };
+
+    // Reviews.belongsTo(models.Users, {
+    //     foreignKey: {
+    //         allowNull: false
+    //     }
+    // })
+
+
+
+    return Reviews;
+}
